393e9fe46dafef3e024576ea7332ac347177a327
Mike <mrgcohen@gmail.com>
2017-01-14 16:03:11 -0500

    Update unimag-swiper.js

--- www/unimag-swiper.js
+++ www/unimag-swiper.js
@@ -55,7 +55,7 @@ Swiper.fireEvent = function (event, data) {
 };
 
 Swiper.on = function (event, callback, scope) {
-	window.removeEventListener(event, callback.find(scope || window));
+	window.removeEventListener(event, callback.bind(scope || window));
 	window.addEventListener(event, callback.bind(scope || window));
 };
 
